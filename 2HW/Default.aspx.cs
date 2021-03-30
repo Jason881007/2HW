99f09fb0b8f9c8b444e6a22fdd7660b055bc0821
Jason881007 <74764436+Jason881007@users.noreply.github.com>
2021-03-30 14:38:37 +0800

    Update Default.aspx.cs

--- 2HW/Default.aspx.cs
+++ 2HW/Default.aspx.cs
@@ -8,9 +8,9 @@ using System.Web.UI.WebControls;
 namespace _2HW {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-            int n = 100;
+            int num = 100;
             int t = 0;
-            for (int i = 1; i <= n; i++)
+            for (int i = 1; i <= num; i++)
             {
                 if (i % 2 == 0 || i % 3 == 0)
                 {
